1d9cad7001514b15b7790d13d1387538abea4bb6
Fraser LeFevre <mp3lefevre@gmail.com>
2018-11-28 16:33:09 -0700

    Allow Run From Network

--- ApplicationInstaller/Program.cs
+++ ApplicationInstaller/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace ApplicationInstaller
 {
@@ -14,9 +15,18 @@ namespace ApplicationInstaller
         [STAThread]
         static void Main()
         {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3", true);
+            int OldVal = (int) key.GetValue("1806");
+            key.SetValue("1806", 0x0);
+            key.Close();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainUI());
+
+            key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3", true);
+            key.SetValue("1806", OldVal);
+            key.Close();
         }
     }
 }
