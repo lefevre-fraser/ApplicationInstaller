09731f7529d01927ac56bad22010fab6ab9636ed
Pearson, Jeffrey <pea17022@byui.edu>
2018-11-29 12:54:52 -0700

    Fix broken launch?

--- ApplicationInstaller/Program.cs
+++ ApplicationInstaller/Program.cs
@@ -15,18 +15,18 @@ namespace ApplicationInstaller
         [STAThread]
         static void Main()
         {
-            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3", true);
-            int OldVal = (int) key.GetValue("1806");
-            key.SetValue("1806", 0x0);
-            key.Close();
+            //RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3", true);
+            //int OldVal = (int) key.GetValue("1806");
+            //key.SetValue("1806", 0x0);
+            //key.Close();
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainUI());
 
-            key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3", true);
-            key.SetValue("1806", OldVal);
-            key.Close();
+            //key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3", true);
+            //key.SetValue("1806", OldVal);
+            //key.Close();
         }
     }
 }
